175786938edf9b7df92894c3092c24a4585ee27d
Alex Zaharia <alex.zaharia24@gmail.com>
2020-10-30 13:53:09 +0200

    min cost for tickets with dp by extending the dp with 30 days

--- google/hw-12/minimum-cost-for-tickets.js
+++ google/hw-12/minimum-cost-for-tickets.js
@@ -1,6 +1,41 @@
 // https://leetcode.com/problems/minimum-cost-for-tickets/
 
 function mincostTickets(days, costs) {
+    return minCostTicketDPWithExtendedDaysArray(days, costs);
+}
+
+function minCostTicketDPWithExtendedDaysArray(days, costs) {
+    // Time: O(n)
+    // Space: O(n)
+
+    // dp[i] = min cost of ticket until day i
+    // dp[i] = min(
+    //      dp[i-1] if i not a travel day,
+    //      dp[i-1] + cost[0],
+    //      dp[i-7] + cost[1] if i>=7,
+    //      dp[i-30] + cost[2] if i>=30
+    // )
+    if (days.length === 0) return 0;
+    let dp = new Array(days[days.length-1] + 31).fill(0);
+    let daysMap = {};
+    for(let day of days) {
+        daysMap[day] = true;
+    }
+
+    dp[0] = 0;
+    for (let i = 1; i < dp.length; i++) {
+        let lastValue = Infinity;
+        if (!(i in daysMap)) {
+            lastValue = dp[i - 1];
+        }
+        dp[i] = Math.min(lastValue, dp[i - 1] + costs[0], (dp[i - 7] || 0) + costs[1], (dp[i - 30] || 0) + costs[2]);
+    }
+    return dp[dp.length - 1];
+}
+
+function mincostTicketsDPWithCostReplacement(days, costs) {
+    // Sounds good, doesn't work
+
     if (days.length === 0) return 0;
     let dp = new Array(days.length).fill(0);
 
@@ -9,15 +44,15 @@ function mincostTickets(days, costs) {
     let startLastCost1 = -1, startLastCost2 = -1;
     let endLastCost1 = -1, endLastCost2 = -1;
 
-    let reasonableUsesOfCost0 = 3;
-    let reasonableUsesOfCost1 = 14;
+    // let reasonableUsesOfCost0 = 3;
+    // let reasonableUsesOfCost1 = 15;
 
-    // console.log(days)
+    let reasonableUsesOfCost0 = Math.floor(costs[1] / costs[0]);
+    console.log(`reasonableUsesOfCost0: ${reasonableUsesOfCost0}`)
+    let reasonableUsesOfCost1 = Math.floor(costs[2] / costs[0]);
+    console.log(`reasonableUsesOfCost1: ${reasonableUsesOfCost1}`)
+
     for (let i = 1; i < days.length; i++) {
-        // console.log(dp)
-        // console.log(startLastCost1)
-        // console.log(startLastCost2)
-        // See if we can apply cost[2]
         if (startLastCost2 !== -1) {
             // cost[2] was applied for dp[i-1]. Try to apply it for dp[i] as well
             if (days[i] - days[startLastCost2] < 30) {
@@ -26,7 +61,7 @@ function mincostTickets(days, costs) {
             } else {
                 startLastCost2 = -1;
                 endLastCost2 = i - 1;
-                dp[i] = dp[i - 1] + 2;
+                dp[i] = dp[i - 1] + costs[0];
             }
         } else if ((endLastCost2 === -1 || days[i] - days[endLastCost2] >= 30) && (i - reasonableUsesOfCost1 >= 0 && days[i] - days[i - reasonableUsesOfCost1] < 30)) {
             // We can use cost[2]
@@ -36,9 +71,9 @@ function mincostTickets(days, costs) {
                 endLastCost1 = i - 1;
             }
             if (i - reasonableUsesOfCost1 === 0) {
-                dp[i] = 15;
+                dp[i] = costs[2];
             } else {
-                dp[i] = dp[i - reasonableUsesOfCost1 - 1] + 15;
+                dp[i] = dp[i - reasonableUsesOfCost1 - 1] + costs[2];
             }
         } else if (startLastCost1 !== -1) {
             // cost[1] was applied for dp[i-1]. See if we can apply it to dp[i] as well
@@ -48,7 +83,7 @@ function mincostTickets(days, costs) {
             } else {
                 startLastCost1 = -1;
                 endLastCost1 = i - 1;
-                dp[i] = dp[i - 1] + 2;
+                dp[i] = dp[i - 1] + costs[0];
             }
         } else if ((endLastCost1 === -1 || days[i] - days[endLastCost1] >= 7) && (i - reasonableUsesOfCost0 >= 0 && days[i] - days[i - reasonableUsesOfCost0] < 7)) {
             // We can apply cost[1]
@@ -59,11 +94,18 @@ function mincostTickets(days, costs) {
                 dp[i] = dp[i - reasonableUsesOfCost0 - 1] + 7;
             }
         } else {
-            dp[i] = dp[i - 1] + 2;
+            dp[i] = dp[i - 1] + costs[0];
         }
     }
+    let output = "";
+    for (let i = 0; i < days.length; i++) {
+        output += `${days[i]}: ${dp[i]}` + " ";
+    }
+    console.log(output);
+
     return dp[days.length - 1];
 }
 
 console.log(mincostTickets([1, 4, 6, 7, 8, 20], [2, 7, 15]))
 console.log(mincostTickets([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31], [2, 7, 15]))
+console.log(mincostTickets([1, 4, 6, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 23, 27, 28], [3, 13, 45]))
