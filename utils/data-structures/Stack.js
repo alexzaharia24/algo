9bcccd445be94f2a23df5b04d07466b3479d3578
Alex Zaharia <alex.zaharia24@gmail.com>
2021-08-06 11:24:59 +0300

    Stack implementations with array and linked list

---
+++ utils/data-structures/Stack.js
@@ -0,0 +1,75 @@
+class StackWithArray {
+    constructor() {
+        this.stack = [];
+    }
+
+    push(item) {
+        this.stack.push(item);
+    }
+
+    pop() {
+        if(this.isEmpty()) throw new Error("Stack is empty, cannot pop.");
+        return this.stack.pop();
+    }
+
+    peek() {
+        if(this.isEmpty()) throw new Error("Stack is empty, cannot peek.");
+        return this.stack[this.stack.length-1];
+    }
+
+    isEmpty() {
+        return this.stack.length === 0;
+    }
+}
+
+class StackNode {
+    constructor(value, next) {
+        this.value = value ?? null;
+        this.next = next ?? null;
+    }
+}
+class StackWithLinkedList {
+    constructor() {
+        this.top = null;
+    }
+
+    push(item) {
+        let newNode = new StackNode(item, this.top);
+        this.top = newNode;
+    }
+
+    pop() {
+        if(this.isEmpty()) {
+            throw new Error("Stack is empty, cannot pop.");
+        } 
+
+        let item = this.top.value;
+        this.top = this.top.next;
+        return item;
+    }
+
+    peek() {
+        if(this.isEmpty()) {
+            throw new Error("Stack is empty, cannot peek.");
+        } 
+
+        return this.top.value;
+    }
+
+
+    isEmpty() {
+        return this.top === null;
+    }
+}
+
+let stack = new StackWithLinkedList();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+console.log(stack.top);
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.peek());
+console.log(stack.isEmpty());
+console.log(stack.pop());
+console.log(stack.isEmpty());
