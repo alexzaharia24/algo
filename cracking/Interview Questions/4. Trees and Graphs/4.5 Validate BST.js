f6c3a75162e06e9c3d9def8e4fb0ff0166776674
Alex Zaharia <alex.zaharia24@gmail.com>
2021-09-13 16:34:21 +0300

    Updated with alterantive solutions: in-order && with range

--- cracking/Interview Questions/4. Trees and Graphs/4.5 Validate BST.js
+++ cracking/Interview Questions/4. Trees and Graphs/4.5 Validate BST.js
@@ -32,30 +32,29 @@ function minValue(root, mem) {
     return mem.get(root);
 }
 
-let n0 = new BinaryTreeNode(0);
-let n1 = new BinaryTreeNode(1);
-let n2 = new BinaryTreeNode(2);
-let n3 = new BinaryTreeNode(3);
-let n4 = new BinaryTreeNode(4);
-let n5 = new BinaryTreeNode(5);
-let n6 = new BinaryTreeNode(6);
+// Alternative solution 1 (when you don't have duplicates): Use the in-order traversal order and check if elements are sorted => valid BST
+// p247
 
-n0.left = n1;
-n0.right = n2;
-n1.right = n3;
-n2.left = n4;
-n3.left = n5;
-n5.left = n6;
+// Alternative solution 2: left <= current < right but instead of memoization use a range min,max that you pass down in the recursion
 
-console.log(validateBST(n0)); // False
+function validateBSTWithRange(root) {
+    return validateBSTWithRange(root, -Infinity, Infinity);
+}
 
-// let n0 = new BinaryTreeNode(10);
-// let n1 = new BinaryTreeNode(6);
-// let n2 = new BinaryTreeNode(15);
-// let n3 = new BinaryTreeNode(9);
-// let n4 = new BinaryTreeNode(11);
-// let n5 = new BinaryTreeNode(8);
-// let n6 = new BinaryTreeNode(7);
+function validateBSTWithRange(root, min, max) {
+    // root.value must be > min && <= max
+    if(root == null) return true;
+    if(root.value <= min || root.value > max) return false;
+    return validateBSTWithRange(root.left, min, root.value) && validateBSTWithRange(root.right, root.value, max);
+}
+
+// let n0 = new BinaryTreeNode(0);
+// let n1 = new BinaryTreeNode(1);
+// let n2 = new BinaryTreeNode(2);
+// let n3 = new BinaryTreeNode(3);
+// let n4 = new BinaryTreeNode(4);
+// let n5 = new BinaryTreeNode(5);
+// let n6 = new BinaryTreeNode(6);
 
 // n0.left = n1;
 // n0.right = n2;
@@ -64,4 +63,21 @@ console.log(validateBST(n0)); // False
 // n3.left = n5;
 // n5.left = n6;
 
-// console.log(validateBST(n0)); // True
+// console.log(validateBSTWithRange(n0)); // False
+
+let n0 = new BinaryTreeNode(10);
+let n1 = new BinaryTreeNode(6);
+let n2 = new BinaryTreeNode(15);
+let n3 = new BinaryTreeNode(9);
+let n4 = new BinaryTreeNode(11);
+let n5 = new BinaryTreeNode(8);
+let n6 = new BinaryTreeNode(7);
+
+n0.left = n1;
+n0.right = n2;
+n1.right = n3;
+n2.left = n4;
+n3.left = n5;
+n5.left = n6;
+
+console.log(validateBSTWithRange(n0)); // True
