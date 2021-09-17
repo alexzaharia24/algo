a96dddb80136084e77983a7e3927695d6146face
Alex Zaharia <alex.zaharia24@gmail.com>
2021-09-17 21:23:24 +0300

    Random shuffle with brute and Fisher Yates

--- leetcode/solved/384_shuffle_an_array/shuffle_an_array.js
+++ leetcode/solved/384_shuffle_an_array/shuffle_an_array.js
@@ -1,50 +1,69 @@
 var Solution = function (nums) {
-    this.permutations = this.getPermutations(nums);
-    this.original = nums;
-  };
-  
-  /**
-  * Resets the array to its original configuration and return it.
-  * @return {number[]}
-  */
-  Solution.prototype.reset = function () { 
-    this.permutations = this.getPermutationsRec(this.original);
+    this.original = [...nums];
+};
+
+/**
+* Resets the array to its original configuration and return it.
+* @return {number[]}
+*/
+Solution.prototype.reset = function () {
     return this.original;
-  };
-  
-  /**
-  * Returns a random shuffling of the array.
-  * @return {number[]}
-  */
-  Solution.prototype.shuffle = function () {
-    let perm = this.permutations.pop();
-    this.permutations.splice(0,0,perm);
+};
+
+/**
+* Returns a random shuffling of the array.
+* @return {number[]}
+*/
+Solution.prototype.shuffle = function () {
+    return shuffleFisherYates(this.original);
+};
+
+function shuffleBruteForce(nums) {
+    let perm = [];
+    let remaining = [...nums];
+    for (let i = 0; i < nums.length; i++) {
+        let randomIdx = parseInt(Math.random() * remaining.length);
+        // console.log(remaining, perm, randomIdx)
+        perm.push(remaining[randomIdx]);
+        remaining.splice(randomIdx, 1);
+    }
     return perm;
-  };
-  
-  Solution.prototype.getPermutations = function(arr) {
+}
+
+// https://leetcode.com/problems/shuffle-an-array/solution/
+function shuffleFisherYates(nums) {
+    let perm = [...nums];
+
+    for (let i = 0; i < perm.length; i++) {
+        let randomIdx = parseInt(i + Math.random() * (perm.length - i));
+        [perm[i], perm[randomIdx]] = [perm[randomIdx], perm[i]];
+    }
+
+    return perm;
+}
+
+Solution.prototype.getPermutations = function (arr) {
     let permutations = [];
     this.getPermutationsRec(arr, [], permutations);
     return permutations;
-  }
-  
-  Solution.prototype.getPermutationsRec = function(arr, currentPerm, perms) {
+}
+
+Solution.prototype.getPermutationsRec = function (arr, currentPerm, perms) {
     // console.log(arr, currentPerm, perms);
     if (arr.length === 0) {
-      perms.push(currentPerm);
-      return;
+        perms.push(currentPerm);
+        return;
     }
-  
+
     for (let i = 0; i < arr.length; i++) {
-      let newArr = [...arr];
-      newArr.splice(i,1);
-      this.getPermutationsRec(newArr, [...currentPerm, arr[i]], perms);
+        let newArr = [...arr];
+        newArr.splice(i, 1);
+        this.getPermutationsRec(newArr, [...currentPerm, arr[i]], perms);
     }
-  }
-  
-  // console.log(getPermutations([1,2,3]));
-  
-  let x = new Solution([1, 2, 3])
-  console.log(x.shuffle());
-  console.log(x.shuffle());
-  console.log(x.shuffle());
+}
+
+
+let x = new Solution([1, 2, 3])
+console.log(x.shuffle());
+console.log(x.shuffle());
+console.log(x.shuffle());
