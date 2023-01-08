34a9549f003671596a52ca9c2ac4ba6f9f093459
Alex Zaharia <alex.zaharia24@gmail.com>
2023-01-08 08:55:49 +0200

    contains duplicate use maps

--- leetcode/solved/121_contains_duplicate/solve.js
+++ leetcode/solved/121_contains_duplicate/solve.js
@@ -12,12 +12,27 @@ const containsDuplicate = nums => {
     return false;
 }
 
+const containsDuplicateWithMap = nums => {
+    // 1. two fors O(n^2) O(1)
+    // 2. one for and hashmap O(n) O(n)
+    const map = new Map();
+    for(let i=0; i<nums.length; i++) {
+        if(map.get(nums[i])) {
+            return true;
+        }
+        map.set(nums[i], true);
+    }
+
+    return false;
+}
+
+
 // [1, 2, 3, 4] => false
 /* eslint-disable */
-console.log(`🔥 solve.js:16 :`, containsDuplicate([1,2,3,4]));
+console.log(`🔥 solve.js:16 :`, containsDuplicateWithMap([1,2,3,4]));
 // [1, 1, 2, 3] => true
 /* eslint-disable */
-console.log(`🔥 solve.js:19 :`, containsDuplicate([1,1,2,3]));
+console.log(`🔥 solve.js:19 :`, containsDuplicateWithMap([1,1,2,3]));
 // [1, 2, 2, 3, 4, 4, 5] => true
 /* eslint-disable */
-console.log(`🔥 solve.js:22 containsDuplicate([1,2,2,3,4,4,5]):`, containsDuplicate([1,2,2,3,4,4,5]));
+console.log(`🔥 solve.js:22 containsDuplicateWithMap([1,2,2,3,4,4,5]):`, containsDuplicateWithMap([1,2,2,3,4,4,5]));
